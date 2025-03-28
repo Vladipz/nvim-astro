b6088bbd7ab414d91940f9080fd8c0ea403295ec
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2025-03-28 10:22:48 +0200

    feat: configure Copilot plugin with new settings

--- init.lua
+++ init.lua
@@ -18,6 +18,10 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,11 +1,32 @@
 return {
+  -- {
+  --   "github/copilot.vim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  --   end,
+  -- },
   {
-    "github/copilot.vim",
-    event = "VeryLazy",
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "User AstroFile",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        debounce = 30,
+        keymap = {
+          accept = "<C-y>",
+          accept_line = "<C-l>",
+        },
+      },
+      copilot_model = "gpt-4o-copilot",
+      filetypes = {
+        markdown = true,
+        gitcommit = true,
+        ["."] = true,
+      },
+    },
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
